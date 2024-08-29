4aa0585743fb60409740098fddc57ade5dd76e6b
NareshBLoc <nareshgdkp@gmail.com>
2024-08-29 15:56:24 +0530

    test Page

--- logintest.js
+++ logintest.js
@@ -1,3 +1,9 @@
 function check(){
-    alert("Its working");
+    var name = document.getElementById("name").value;
+    var pwd = document.getElementById("pwd").value;
+    if(name.toUpperCase() === "NARESH" && pwd.toUpperCase() === "SUCCESSFULL")
+        alert("Welcome my liege!!");
+    else
+        alert("Cease your actions peasant!");
+
 }
